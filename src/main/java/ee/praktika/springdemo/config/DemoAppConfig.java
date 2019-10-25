cb222f334df5fb25f00074e2073d8596b69a170a
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-25 10:37:56 +0300

    Cosmetic updates.

--- src/main/java/ee/praktika/springdemo/config/DemoAppConfig.java
+++ src/main/java/ee/praktika/springdemo/config/DemoAppConfig.java
@@ -13,45 +13,36 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.luv2code.springdemo")
-@PropertySource({ "classpath:application.properties" })
+@ComponentScan( "ee.praktika.springdemo" )
+@PropertySource( { "classpath:application.properties" } )
 public class DemoAppConfig implements WebMvcConfigurer {
 
-	// define a bean for ViewResolver
+    // define a bean for ViewResolver
 
-	@Bean
-	public ViewResolver viewResolver() {
-		
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		
-		viewResolver.setPrefix("/WEB-INF/view/");
-		viewResolver.setSuffix(".jsp");
-		
-		return viewResolver;
-	}
-	
-	// define bean for RestTemplate ... this is used to make client REST calls
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-	
-	// add resource handler for loading css, images, etc
-	
+    @Bean
+    public ViewResolver viewResolver(){
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix( "/WEB-INF/view/" );
+        viewResolver.setSuffix( ".jsp" );
+
+        return viewResolver;
+    }
+
+    // define bean for RestTemplate ... this is used to make client REST calls
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    // add resource handler for loading css, images, etc
+
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers( ResourceHandlerRegistry registry ){
         registry
-          .addResourceHandler("/resources/**")
-          .addResourceLocations("/resources/"); 
-    }	
+            .addResourceHandler( "/resources/**" )
+            .addResourceLocations( "/resources/" );
+    }
 }
-
-
-
-
-
-
-
-
-
